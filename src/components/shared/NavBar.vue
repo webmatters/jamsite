d08111235deb23c7c5fb3ee971ce4e197ce782f2
Andrew Smith <andrew.smith@webmatters.io>
2020-01-18 13:14:45 -0500

    Constrains width of CTA on mobile

--- src/components/shared/NavBar.vue
+++ src/components/shared/NavBar.vue
@@ -1,7 +1,7 @@
 <template>
   <!-- Responsive NavBar -->
   <nav
-    class="bg-white sm:flex sm:justify-between sm:items-center sm:px-4 sm:py-3"
+    class="w-full bg-white sm:flex sm:justify-between sm:items-center sm:px-4 sm:py-3"
   >
     <div class="flex justify-between items-center py-3 px-4 sm:p-0">
       <!-- Brand logo -->
@@ -23,28 +23,29 @@
     <!-- Navigation Links -->
     <div
       :class="isOpen ? 'block' : 'hidden'"
-      class="px-2 pt-2 pb-4 sm:flex text-primary flex items-center"
+      class="px-2 pt-2 pb-4 sm:flex text-primary sm:flex sm:items-center"
     >
       <g-link
-        class="block px-2 py-1 font-semibold rounded hover:text-secondary"
+        class="block px-2 py-2 font-semibold rounded hover:text-secondary"
         to="/services/"
         >Services</g-link
       >
       <g-link
-        class="block mt-1 px-2 py-1 font-semibold rounded hover:text-secondary sm:mt-0 sm:ml-6"
+        class="block mt-1 px-2 py-2 font-semibold rounded hover:text-secondary sm:mt-0 sm:ml-6"
         to="/about/"
         >About</g-link
       >
       <g-link
-        class="block mt-1 px-2 py-1 font-semibold rounded hover:text-secondary sm:mt-0 sm:ml-6"
+        class="block mt-1 px-2 py-2 font-semibold rounded hover:text-secondary sm:mt-0 sm:ml-6"
         to="/portfolio/"
         >Our Work</g-link
       >
       <g-link
-        class="block mt-1 px-4 py-2 font-semibold rounded-full bg-cta text-white hover:bg-cta-hover sm:mt-0 sm:ml-6"
+        class="inline-block mt-3 px-4 py-2 font-semibold rounded-full bg-cta text-white hover:bg-cta-hover sm:mt-0 sm:ml-6"
         to="/contact/"
-        >Free Strategy Session</g-link
       >
+        Free Strategy Session
+      </g-link>
     </div>
   </nav>
 </template>
