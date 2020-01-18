ae6c587fc2585b4f9a403bbf59136cdf3b3f8170
Andrew Smith <andrew.smith@webmatters.io>
2020-01-18 11:48:48 -0500

    Adds linting and formatting configs. Moves navbar to separate component.

---
+++ .eslintrc.js
@@ -0,0 +1,24 @@
+'use strict'
+
+module.exports = {
+  parser: 'vue-eslint-parser',
+  parserOptions: {
+    ecmaVersion: 2018,
+    sourceType: 'module',
+    ecmaFeatures: {
+      jsx: true,
+    },
+  },
+  env: {
+    browser: true,
+    es6: true,
+  },
+  plugins: ['prettier', 'gridsome'],
+  extends: ['plugin:prettier/recommended', 'plugin:gridsome/recommended'],
+  rules: {
+    'gridsome/format-query-block': 'warn',
+    'gridsome/require-g-image-src': 'error',
+    'gridsome/require-g-link-to': 'warn',
+    'prettier/prettier': 'error',
+  },
+}
--- gridsome.config.js
+++ gridsome.config.js
@@ -1,25 +1,25 @@
-const tailwind = require("tailwindcss");
-const purgecss = require("@fullhuman/postcss-purgecss");
-const autoprefixer = require("autoprefixer");
+const tailwind = require('tailwindcss')
+const purgecss = require('@fullhuman/postcss-purgecss')
+const autoprefixer = require('autoprefixer')
 
-const postcssPlugins = [tailwind(), autoprefixer()];
+const postcssPlugins = [tailwind(), autoprefixer()]
 
-if (process.env.NODE_ENV === "production") postcssPlugins.push(purgecss());
+if (process.env.NODE_ENV === 'production') postcssPlugins.push(purgecss())
 
 module.exports = {
-  siteName: "Web Matters",
-  siteUrl: "https://www.webmatters.io",
+  siteName: 'Web Matters',
+  siteUrl: 'https://www.webmatters.io',
   plugins: [],
   css: {
     loaderOptions: {
       postcss: {
-        plugins: postcssPlugins
-      }
-    }
+        plugins: postcssPlugins,
+      },
+    },
   },
   chainWebpack: config => {
-    const svgRule = config.module.rule("svg");
-    svgRule.uses.clear();
-    svgRule.use("vue-svg-loader").loader("vue-svg-loader");
-  }
-};
+    const svgRule = config.module.rule('svg')
+    svgRule.uses.clear()
+    svgRule.use('vue-svg-loader').loader('vue-svg-loader')
+  },
+}
